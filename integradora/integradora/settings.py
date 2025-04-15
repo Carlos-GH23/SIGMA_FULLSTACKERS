f1293e0375c36440b671044027d59e505d98bab9
sebas <sebastian.quinteromtz@gmail.com>
2025-04-14 22:57:26 -0600

    Commit

--- integradora/integradora/settings.py
+++ integradora/integradora/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Gestion_vehicular',
         'USER': 'root',
-        'PASSWORD': '2003',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
